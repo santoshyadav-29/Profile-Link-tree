cd3917f8a7bae570e2cfb944edf00f7797b08c17
Santosh <sy59021321@student.ku.edu.np>
2023-03-08 23:08:39 +0545

    dark mode

---
+++ index.js
@@ -0,0 +1,9 @@
+const background=document.querySelector('body');
+const btn=document.querySelector('section button');
+btn.addEventListener('click',()=>{
+    background.classList.toggle('darkmode');
+
+    
+
+})
+
